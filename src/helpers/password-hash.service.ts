e1afdc2fd6c95645868326195abb9c1b62b5857a
Rajeev Rajchal <rajeevrajchal12@gmail.com>
2023-12-16 21:50:00 +0545

    Feature:  Added email to mailtrap

--- src/helpers/password-hash.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
-
-@Injectable()
-export class PasswordHashService {
-  async hashData(data: string): Promise<string> {
-    return bcrypt.hash(data, 10);
-  }
-}
