19b22225460a33ae1ead3fa6050bc0ae87448ab0
Rajeev Rajchal <rajeevrajchal12@gmail.com>
2024-02-25 22:16:38 +0545

    Bugfix: Added module for service

--- src/module/project/project.module.ts
+++ src/module/project/project.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
+import { SlugService } from 'src/helpers/slug-generator.service';
+import { SolarService } from 'src/helpers/solar.service';
+import { UserCheckerService } from 'src/helpers/user-checker.service';
+import { JwtAuthGuard } from 'src/middleware/guard/jwt-auth.guard';
+import { RolesGuard } from 'src/middleware/guard/role.guard';
+import { CloudinaryService } from '../cloudinary/cloudinary.service';
+import { MailService } from '../mail/mail.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { ProjectController } from './project.controller';
 import { ProjectService } from './project.service';
-import { PrismaService } from '../prisma/prisma.service';
-import { RolesGuard } from 'src/middleware/guard/role.guard';
-import { JwtAuthGuard } from 'src/middleware/guard/jwt-auth.guard';
-import { UserCheckerService } from 'src/helpers/user-checker.service';
-import { SlugService } from 'src/helpers/slug-generator.service';
-import { MailService } from '../mail/mail.service';
-import { SolarService } from 'src/helpers/solar.service';
-import { CloudinaryService } from '../cloudinary/cloudinary.service';
 
 @Module({
   imports: [],
--- src/module/service/service.controller.ts
+++ src/module/service/service.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { CustomerService, Service } from '@prisma/client';
+import { JwtAndRolesGuard } from 'src/middleware/guard/jwt-auth-role.guard';
+import { QueryParamsDto } from '../inventory/args/query-decorators';
+import { ServiceService } from './service.service';
 
+@UseGuards(JwtAndRolesGuard)
 @Controller('service')
-export class ServiceController {}
+export class ServiceController {
+  constructor(private readonly serviceService: ServiceService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async all(@Query() query: QueryParamsDto): Promise<Service[]> {
+    return this.serviceService.all(query);
+  }
+
+  @Get('customer-configuration')
+  @HttpCode(HttpStatus.OK)
+  async booked(@Query() query: QueryParamsDto): Promise<CustomerService[]> {
+    return this.serviceService.booked(query);
+  }
+}
